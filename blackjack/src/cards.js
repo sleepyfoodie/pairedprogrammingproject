b402b3ab37ef0cf299da4d322b3d4b0c1b246dbe
sleepyfoodie <cerynaa@gmail.com>
2017-08-08 15:59:48 -0400

    Pulling changes

--- blackjack/src/cards.js
+++ blackjack/src/cards.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 
 
 class Cards extends Component {
+  constructor(){
+    super()
+    this.state = {
+       deck: {
+         twoclub: {
+           path: "/cards/2_of_clubs.png",
+           suit: "club",
+           value: 2
+         },
+          twodiamond:{
+            path: "/cards/2_of_diamonds.png",
+            suit: "diamond",
+            value: 2
+          }
+       }
+    }
+  }
   render() {
     return (
       <div>
+        <img src="{this.state.deck.twoclub.path}"/>
 
       </div>
     );
