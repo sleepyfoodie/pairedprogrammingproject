99e5a382fd44f6c5aa52cbe5de5c96b5a60bd224
sleepyfoodie <cerynaa@gmail.com>
2017-08-10 23:45:02 -0400

    removed images path since it's useless now

--- blackjack/src/cards.js
+++ blackjack/src/cards.js
@@ -61,263 +61,211 @@ class Cards extends Component {
             deck: [
                 {
                     name: 'twoofclubs',
-                    value: 2,
-                    image: '/cards/2_of_clubs.png'
+                    value: 2
                 },
                 {
                     name: 'twoofdiamonds',
-                    value: 2,
-                    image: '/cards/2_of_diamonds.png'
+                    value: 2
                 },
                 {
                     name: 'twoofhearts',
-                    value: 2,
-                    image: '/cards/2_of_hearts.png'
+                    value: 2
                 },
                 {
                     name: 'twoofspades',
-                    value: 2,
-                    image: '/cards/2_of_spades.png'
+                    value: 2
                 },
                 {
                     name: 'threeofclubs',
-                    value: 3,
-                    image: '/cards/3_of_clubs.png'
+                    value: 3
                 },
                 {
                     name: 'threeofdiamonds',
-                    value: 3,
-                    image: '/cards/3_of_diamonds.png'
+                    value: 3
                 },
                 {
                     name: 'threeofhearts',
-                    value: 3,
-                    image: '/cards/3_of_hearts.png'
+                    value: 3
                 },
                 {
                     name: 'threeofspades',
-                    value: 3,
-                    image: '/cards/3_of_spades.png'
+                    value: 3
                 },
                 {
                     name: 'fourofclubs',
-                    value: 4,
-                    image: '/cards/4_of_clubs.png'
+                    value: 4
                 },
                 {
                     name: 'fourofdiamonds',
-                    value: 4,
-                    image: '/cards/4_of_diamonds.png'
+                    value: 4
                 },
                 {
                     name: 'fourofhearts',
-                    value: 4,
-                    image: '/cards/4_of_hearts.png'
+                    value: 4
                 },
                 {
                     name: 'fourofspades',
-                    value: 4,
-                    image: '/cards/4_of_spades.png'
+                    value: 4
                 },
                 {
                     name: 'fiveofclubs',
-                    value: 5,
-                    image: '/cards/5_of_clubs.png'
+                    value: 5
                 },
                 {
                     name: 'fiveofdiamonds',
-                    value: 5,
-                    image: '/cards/5_of_diamonds.png'
+                    value: 5
                 },
                 {
                     name: 'fiveofhearts',
-                    value: 5,
-                    image: '/cards/5_of_hearts.png',
+                    value: 5
                 },
                 {
                     name: 'fiveofspades',
-                    value: 5,
-                    image: '/cards/5_of_spades.png',
+                    value: 5
                 },
                 {
                     name: 'sixofclubs',
-                    value: 6,
-                    image: '/cards/6_of_clubs.png',
+                    value: 6
                 },
                 {
                     name: 'sixofdiamonds',
-                    value: 6,
-                    image: '/cards/6_of_diamonds.png',
+                    value: 6
                 },
                 {
                     name: 'sixofhearts',
-                    value: 6,
-                    image: '/cards/6_of_hearts.png',
+                    value: 6
                 },
                 {
                     name: 'sixofspades',
-                    value: 6,
-                    image: '/cards/6_of_spades.png',
+                    value: 6
                 },
                 {
                     name: 'sevenofclubs',
-                    value: 7,
-                    image: '/cards/7_of_clubs.png',
+                    value: 7
                 },
                 {
                     name: 'sevenofdiamonds',
-                    value: 7,
-                    image: '/cards/7_of_diamonds.png',
+                    value: 7
                 },
                 {
                     name: 'sevenofhearts',
-                    value: 7,
-                    image: '/cards/7_of_hearts.png',
+                    value: 7
                 },
                 {
                     name: 'sevenofspades',
-                    value: 7,
-                    image: '/cards/7_of_spades.png',
+                    value: 7
                 },
                 {
                     name: 'eightofclubs',
-                    value: 8,
-                    image: '/cards/8_of_clubs.png',
+                    value: 8
                 },
                 {
                     name: 'eightofdiamonds',
-                    value: 8,
-                    image: '/cards/8_of_diamonds.png',
+                    value: 8
                 },
                 {
                     name: 'eightofhearts',
-                    value: 8,
-                    image: '/cards/8_of_hearts.png',
+                    value: 8
                 },
                 {
                     name: 'eightofspades',
-                    value: 8,
-                    image: '/cards/8_of_spades.png',
+                    value: 8
                 },
                 {
                     name: "nineofclubs",
-                    value: 9,
-                    image: "/cards/9_of_clubs.png"
+                    value: 9
                 },
                 {
                     name: "nineofdiamonds",
-                    value: 9,
-                    image: "/cards/9_of_diamonds.png"
+                    value: 9
                 },
                 {   
                     name: "nineofhearts",
-                    value: 9,
-                    image: "/cards/9_of_hearts.png"
+                    value: 9
                 },
                 {
                     name: "nineofspades",
-                    value: 9,
-                    image: "/cards/9_of_spades.png"
+                    value: 9
                 },
                 {
                     name: "tenofclubs",
-                    value: 10,
-                    image: "/cards/10_of_clubs.png"
+                    value: 10
                 },
                 {
                     name: "tenofdiamonds",
-                    value: 10,
-                    image: "/cards/10_of_diamonds.png"
+                    value: 10
                 },
                 {
                     name: "tenofhearts",
-                    value: 10,
-                    image: "/cards/10_of_hearts.png"
+                    value: 10
                 },
                 {
                     name: "tenofspades",
-                    value: 10,
-                    image: "/cards/10_of_spades.png"
+                    value: 10
                 },
                 {
                     name: "aceofclubs",
-                    value: 1,
-                    image: "/cards/ace_of_clubs.png"
+                    value: 1
                 },
                 {
                     name: "aceofdiamonds",
-                    value: 1,
-                    image: "/cards/ace_of_diamonds.png"
+                    value: 1
                 },
                 {
                     name: "aceofhearts",
-                    value: 1,
-                    image: "/cards/ace_of_hearts.png"
+                    value: 1
                 },
                 {
                     name: "aceofspades",
-                    value: 1,
-                    image: "/cards/ace_of_spades.png"
+                    value: 1
                 },
                 {
                     name: "jackofclubs",
-                    value: 10,
-                    image: "/cards/jack_of_clubs.png"
+                    value: 10
                 },
                 {
                     name: "jackofdiamonds",
-                    value: 10,
-                    image: "/cards/jack_of_diamonds.png"
+                    value: 10
                 },
                 {   
                     name: "jackofhearts",
-                    value: 10,
-                    image: "/cards/jack_of_hearts.png"
+                    value: 10
                 },
                 {   
                     name: "jackofspades",
-                    value: 10,
-                    image: "cards/jack_of_spades.png"
+                    value: 10
                 },
                 {
                     name: "kingofclubs",
-                    value: 10,
-                    image: "/cards/king_of_clubs.png"
+                    value: 10
                 },
                 {
                     name: "kingofdiamonds",
-                    value: 10,
-                    image: "/cards/king_of_diamonds.png"
+                    value: 10
                 },
                 {
                     name: "kingofhearts",
-                    value: 10,
-                    image: "/cards/king_of_hearts.png"
+                    value: 10
                 },
                 {
                     name: "kingofspades",
-                    value: 10,
-                    image: "/cards/king_of_spades.png"
+                    value: 10
                 },
                 {
                     name: "queenofclubs",
-                    value: 10,
-                    image: "/cards/queen_of_clubs.png",
+                    value: 10
                 },
                 {
                     name: "queenofdiamonds",
-                    value: 10,
-                    image: "/cards/queen_of_diamonds.png"
+                    value: 10
                 },
                 {
                     name: "queenofhearts",
-                    value: 10,
-                    image: "/cards/queen_of_hearts.png"
+                    value: 10
                 },
                 {
                     name: "queenofspades",
-                    value: 10,
-                    image: "/cards/queen_of_spades/png"
+                    value: 10
                 }
             ]
         }
